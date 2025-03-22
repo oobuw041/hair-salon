14839081050283ef049da20feac9cb6548a1df4f
keno <keno.obuwoma@gmail.com>
2025-03-22 19:13:08 -0400

    responsive design

---
+++ app.js
@@ -0,0 +1,12 @@
+//const contactForm = document.getElementById("contactform")
+
+function showMessage() {
+    alert("Thank you for contacting us! We will get back to you shortly.");
+}
+
+
+function showMessage(event) {
+    event.preventDefault(); // Prevent form submission (which would reload the page)
+    alert("Thank you for booking with us! We will get back to you shortly.");
+    window.location.href = "confirmation.html"; // Redirect after alert
+}
