a0bace27f7192ac65966e10dec235c15593ec451
keno <keno.obuwoma@gmail.com>
2025-03-26 19:43:30 -0400

    responsive design changes

--- index.js
+++ index.js
@@ -52,4 +52,25 @@ if(booKNow){
   });
 
 }
+
+
+const elements = document.querySelectorAll('.popUp');
+
+      
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); 
+    }
+  });
+}, {
+  threshold: 0.5 
 });
+
+
+elements.forEach(element => {
+  observer.observe(element);
+});
+});
+
